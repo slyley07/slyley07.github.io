a3f16273a83d99ddf001c68c79df35030508e3f8
Bradley <showalter.bradley@gmail.com>
2015-09-04 16:27:04 -0400

    Initial commit. Create basic site. Start jQuery

---
+++ public/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    // if (y > 50) {
+    //   $('.scroller').fadeOut(100);
+    // }
+
+    if (y > 250) {
+      $('.top').fadeIn(500);
+    }
+
+    if (y > 500) {
+      $('.middle').slideDown(500);
+    }
+
+    if (y > 750) {
+      $('.wrapLeft').css({
+            display: "inline-block"
+          }).show("slide", { direction: "right" }, 500);
+      $('.wrapRight').css ({
+            display: "inline-block"
+          }).show("slide", {direction: "left"}, 500);
+      console.log('works');
+    }
+  });
+})
