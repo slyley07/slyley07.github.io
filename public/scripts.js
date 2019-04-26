15ca76874d85ad024e652f1453f4e5c2e29cedf4
Bradley <showalter.bradley@gmail.com>
2019-04-26 11:39:23 -0400

    Add in completely new stuff and delete the old portfolio.

---
+++ public/scripts.js
@@ -0,0 +1,111 @@
+var projects = [
+  {
+    'name': 'D3 NYC',
+    'className': 'd3',
+    'link': 'https://d3-nyc.com',
+    'desc': 'I built the entire D3 NYC website using React, and then processed it through Gatsby to create a static site. The project uses multiple, complex components, many of which have various states.',
+    'langs': [
+      'React',
+      'Gatsby'
+    ],
+    'imgs': [
+
+    ]
+  },
+  {
+    'name': 'Spacious',
+    'className': 'spacious',
+    'link': 'https://www.spacious.com',
+    'desc': 'I used Webflow CMS to build the Spacious site, which relies extensively on CSS grid for the modular and modern design.',
+    'langs': [
+      'Weblow'
+     ],
+    'imgs': [
+
+    ]
+  },
+  {
+    'name': 'Saturday Skin',
+    'className': 'ss',
+    'link': 'https://saturdayskin.com/',
+    'desc': 'I worked with AngularJS and Node.JS to build Saturday Skin’s site, and used Mongoose and MongoDB for handling data. I also used Shopify cart API to enable the site to handle checkout and payment.',
+    'langs': [
+      'AngularJS',
+      'Node.JS',
+      'Mongoose',
+      'MongoDB',
+      'Shopify'
+    ],
+    'imgs': [
+
+    ]
+  },
+  {
+    'name': 'Jetblack Landing Pages',
+    'className': 'jb',
+    'link': '',
+    'desc': 'These were simple one-off holiday landing pages that I built for Jetblack using HTML, CSS, and JavaScript.',
+    'langs': [
+      'HTML',
+      'CSS',
+      'JavaScript'
+    ],
+    'imgs': [
+
+    ]
+  },
+  {
+    'name': 'FanDuel Landing Pages',
+    'className': 'fd',
+    'link': '',
+    'desc': 'For the 2019 NBA Playoffs, I built several landing pages for promotions that FanDuel ran with partner companies. These all use basic HTML, CSS, and JavaScript.',
+    'langs': [
+      'HTML',
+      'CSS',
+      'JavaScript'
+    ],
+    'imgs': [
+
+    ]
+  }
+];
+
+projects.forEach(project => {
+  if (project.link !== '') {
+    document.getElementsByClassName('project')[0].innerHTML += '<div class="' + project.className + '"><h4><a href="' + project.link + '">' + project.name + '</a></h4><p>' + project.desc + '</p></div>'
+  } else {
+    document.getElementsByClassName('project')[0].innerHTML += '<div class="' + project.className + '"><h4 class="' + project.className + '">' + project.name + '</h4><p>' + project.desc + '</p></div>'
+  }
+});
+
+let togglePic = (active) => {
+  if (active === 'me') {
+    document.getElementsByClassName('me')[0].style.display = 'none';
+    document.getElementsByClassName('boilermakers')[0].style.display = 'block';
+  } else {
+    document.getElementsByClassName('me')[0].style.display = 'block';
+    document.getElementsByClassName('boilermakers')[0].style.display = 'none';
+  }
+}
+
+// let winWid = window.innerWidth;
+// let lis = document.getElementsByTagName('li');
+//
+// if (winWid < 480) {
+//
+//   for (let i = 0; i < lis.length - 1; i++) {
+//     let text = lis[i].innerHTML.trim();
+//     if (text === 'jQuery' || text === 'Rails') {
+//       continue;
+//     }
+//     lis[i].innerHTML =  text + ', ';
+//   }
+// } else {
+//   for (let i = 0; i < lis.length - 1; i++) {
+//     let text = lis[i].innerHTML.trim();
+//     if (text === 'jQuery' || text === 'Rails') {
+//       continue;
+//     }
+//     lis[i].innerHTML =  text + ', ';
+//   }
+// }
